2678125e7abf70efb63014199f62f63127991544
Anton Gushcha <ncrashed@gmail.com>
2017-11-04 10:03:14 +0300

    Start frontend

---
+++ immortelle-cms-frontend/app/Main.hs
@@ -0,0 +1,8 @@
+module Main where
+
+import Immortelle.CMS.Frontend
+
+import Reflex.Dom
+
+main :: IO ()
+main = mainWidget cmsFrontend
---
+++ immortelle-cms-frontend/src/Immortelle/CMS/Frontend.hs
@@ -0,0 +1,10 @@
+module Immortelle.CMS.Frontend(
+    cmsFrontend
+  ) where
+
+import Reflex
+import Reflex.Dom
+
+-- | Main widget of CMS
+cmsFrontend :: MonadWidget m => m ()
+cmsFrontend = pure ()
