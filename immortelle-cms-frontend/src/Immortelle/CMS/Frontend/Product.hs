53a1e7d537cfaaede5cbab17adfc0f6909908831
Anton Gushcha <ncrashed@gmail.com>
2017-11-05 03:54:05 +0300

    Add widget for patination

--- immortelle-cms-frontend/src/Immortelle/CMS/Frontend/Product.hs
+++ immortelle-cms-frontend/src/Immortelle/CMS/Frontend/Product.hs
@@ -3,8 +3,10 @@ module Immortelle.CMS.Frontend.Product(
   ) where
 
 import Control.Monad (join)
+import Data.List (sortBy)
 import Data.Map.Strict (Map)
 import Data.Monoid ((<>))
+import Data.Ord
 import Data.Text (Text, pack)
 import Immortelle.CMS.API
 import Immortelle.CMS.Frontend.Auth
@@ -16,6 +18,8 @@ import Immortelle.CMS.Types
 import Reflex.Dom
 import Web.Reflex.Bootstrap
 
+import qualified Data.Map.Strict as M
+import qualified Data.Set as S
 import qualified Data.Text as T
 import qualified Data.Text.Read as T
 
@@ -92,7 +96,9 @@ productCreateForm = horizontalForm $ do
    nameD <- _textInput_value <$> formGroupText "Имя изделия" def
    catD <- _dropdown_value <$> formGroupSelect "Категория" PendantLeaf (pure categoryLabels) def
    catDatumD <- fmap join $ widgetHoldDyn $ categoryForm <$> catD
-   creationD <- dayCalendarField "Дата изготовления" def 
+   creationD <- dayCalendarField "Дата изготовления" def
+   patinationD <- patinationForm
+   submitE <- submitButton "Создать"
    pure never
 
 -- | Given the fixed category display form for setting cattegory specific data
@@ -137,3 +143,81 @@ categoryForm c = case c of
     height <- mdoubleField "Длина"
     pure $ BookmarkData <$> width <*> height
   Grand -> pure $ pure GrandData
+
+data PatinationTag = NoPatination
+  | PatRainbow
+  | PatAmmonia
+  | PatAmmoniaBlue
+  | PatSulfur
+  | PatGreen
+  | PatStainedGlass
+  deriving (Eq, Ord, Show, Read, Enum, Bounded)
+
+patinationLabels :: Map PatinationTag Text
+patinationLabels = [
+    (NoPatination, "Без обработки")
+  , (PatRainbow, "Радужная патина")
+  , (PatAmmonia, "Аммиак обычный")
+  , (PatAmmonia, "Аммиак голубой")
+  , (PatSulfur, "Серная патина")
+  , (PatGreen, "Зеленая патина")
+  , (PatStainedGlass, "Витражная краска")
+  ]
+
+colorLabels :: Map Color Text
+colorLabels = [
+    (Red, "Красный")
+  , (Orange, "Оранжевый")
+  , (Yellow, "Желтый")
+  , (Green, "Зеленый")
+  , (LightBlue, "Голубой")
+  , (Blue, "Синий")
+  , (Magenta, "Фиолетовый")
+  ]
+
+-- | Select color from dropdown
+colorField :: forall t m . MonadWidget t m => m (Dynamic t Color)
+colorField = _dropdown_value <$> formGroupSelect "Цвет" Red (pure colorLabels) def
+
+-- | Allows to input many values via dynamic count of simple fields
+manyInputs :: forall t m a . MonadWidget t m => m (Dynamic t a) -> m (Dynamic t [a])
+manyInputs makeField = mdo
+  let makeField' k _ _ = row $ do
+        a <- md10 makeField
+        delE <- md2 $ primaryButton "Удалить"
+        pure (a, delE)
+  tmap :: Dynamic t (Map Int (Dynamic t a, Event t ())) <- listWithKeyShallowDiff [(0, ())] updE makeField'
+  let valmap = joinDynThroughMap $ fmap fst <$> tmap :: Dynamic t (Map Int a)
+      delmap = fmap snd <$> tmap :: Dynamic t (Map Int (Event t ()))
+      delmap' = switchPromptlyDyn $ mergeMap <$> delmap :: Event t (Map Int ())
+      delmap'' = fmap (const Nothing) <$> delmap' :: Event t (Map Int (Maybe ()))
+      maximum' :: [Int] -> Int
+      maximum' [] = 0
+      maximum' xs = maximum xs
+      lastid = maximum' . M.keys <$> tmap :: Dynamic t Int
+  addE <- row $ md1 (pure ()) >> md2 (primaryButton "Больше элементов")
+  let addE' = attachPromptlyDynWith (\i _ -> [(i+1, Just ())]) lastid addE :: Event t (Map Int (Maybe ()))
+      updE = delmap'' <> addE'
+  pure $ fmap snd . sortBy (comparing fst) . M.toList <$> valmap
+  where
+    md1 = elClass "div" "col-md-1"
+    md10 = elClass "div" "col-md-10"
+
+-- | Part of form that allows dynamically input patination types with colors
+patinationForm :: MonadWidget t m => m (Dynamic t (Maybe Patination))
+patinationForm = do
+  tagD <- _dropdown_value <$> formGroupSelect "Тип патины" NoPatination (pure patinationLabels) def
+  fmap join $ widgetHoldDyn $ makeForm <$> tagD
+  where
+    makeForm tg = case tg of
+      NoPatination -> pure $ pure Nothing
+      PatRainbow -> do
+        clrs <- fmap S.fromList <$> manyInputs colorField
+        pure $ Just . PatinationRainbow <$> clrs
+      PatAmmonia -> pure . pure . Just $ PatinationAmmonia
+      PatAmmoniaBlue -> pure . pure . Just $ PatinationAmmoniaBlue
+      PatSulfur -> pure . pure . Just $ PatinationSulfur
+      PatGreen -> pure . pure . Just $ PatinationGreen
+      PatStainedGlass -> do
+        clrs <- fmap S.fromList <$> manyInputs colorField
+        pure $ Just . StainedGlassPaint <$> clrs
